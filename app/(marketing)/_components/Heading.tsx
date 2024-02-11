b03d09452b7c2ddb171427760ef7a5133e3ec588
arihant-jain0 <jainari1208@gmail.com>
2024-02-11 16:42:44 +0530

    🚧 Marketing page

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Heading = () => {
+  return (
+ <div className="max-w-3xl space-y-4">
+    <h1>
+        Welcome to the Motion-Notion clones
+    </h1>
+ </div>
+  )
+}
+
+export default Heading
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,14 @@
+import Heading from "./_components/Heading"
+
+
+const page = () => {
+  return (
+    <div className="min-h-full flex flex-col ">
+        <div className="flex flex-col items-center justify-center gap-y-8 flex-1 px-6 pb-10 md:justify-start text-center">
+            <Heading/>
+        </div>
+    </div>
+  )
+}
+
+export default page
