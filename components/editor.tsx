6712b56ec773343553ddec1c4c61b9fa516b20d8
arihant-jain0 <jainari1208@gmail.com>
2024-02-14 16:26:00 +0530

    solve issue in next config

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,51 +1,43 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import {
-  BlockNoteEditor,
-  PartialBlock
-} from "@blocknote/core";
-import {
-  BlockNoteView,
-  useBlockNote
-} from "@blocknote/react";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
 
 import { useEdgeStore } from "@/lib/edgestore";
 
+// Ensure that PartialBlock is properly imported and defined
 interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
   editable?: boolean;
-};
+}
 
-const Editor = ({
-  onChange,
-  initialContent,
-  editable
-}: EditorProps) => {
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
   const { edgestore } = useEdgeStore();
 
   const handleUpload = async (file: File) => {
-    const response = await edgestore.publicFiles.upload({ 
-      file
+    const response = await edgestore.publicFiles.upload({
+      file,
     });
 
     return response.url;
-  }
+  };
 
+  // Ensure that PartialBlock is properly imported and defined
   const editor: BlockNoteEditor = useBlockNote({
     editable,
-    initialContent: 
-      initialContent 
-      ? JSON.parse(initialContent) as PartialBlock[] 
-      : undefined,
+    initialContent:
+      initialContent
+        ? JSON.parse(initialContent) 
+        : undefined,
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
-    uploadFile: handleUpload
-  })
+    uploadFile: handleUpload,
+  });
 
   return (
     <div>
@@ -54,7 +46,7 @@ const Editor = ({
         theme={resolvedTheme === "dark" ? "dark" : "light"}
       />
     </div>
-  )
-}
+  );
+};
 
 export default Editor;
