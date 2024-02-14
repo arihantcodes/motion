84ce51522ad9788a03b1f3654c05ccb23035e0ac
arihant-jain0 <jainari1208@gmail.com>
2024-02-14 16:37:24 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
               defaultTheme="system"
               enableSystem
               disableTransitionOnChange
-              storageKey="jotion-theme-2"
+              storageKey="Motion-theme-2"
             >
               <Toaster position="bottom-center" />
               <ModalProvider />
